1490e877db44f1149c5c7f466363c5330f6c60f9
skYth1an <Elgatomolli27072908>
2023-01-15 12:08:53 +0300

    add folderid

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ terraform {
 
 resource "yandex_iam_service_account" "sa" {
   name = "k8s"
+  folder_id      = "${var.folder_id}"
 }
 
 // Назначение роли сервисному аккаунту
@@ -85,6 +86,7 @@ resource "yandex_vpc_subnet" "public" {
   v4_cidr_blocks = ["192.168.10.0/24"]
   zone           = "${var.zone}"
   network_id     = "${yandex_vpc_network.network.id}"
+  folder_id      = "${var.folder_id}"
 }
 
 
