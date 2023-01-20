b802169b62230dc7371d4512f0af608c587d1973
skYth1an <Elgatomolli27072908>
2023-01-20 21:37:29 +0300

    change main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "yandex_compute_instance" "kuber" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd8jvcoeij6u9se84dt5"
+      image_id = "fd8ueg1g3ifoelgdaqhb"
       size = 20
     }
   }
@@ -75,7 +75,7 @@ resource "yandex_compute_instance" "kuber2" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd8jvcoeij6u9se84dt5"
+      image_id = "fd8ueg1g3ifoelgdaqhb"
       size = 20
     }
   }
